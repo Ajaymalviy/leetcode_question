5913e4ce0e0e9520c598b7663b590af2a29024de
Ajaymalviy <128165665+Ajaymalviy@users.noreply.github.com>
2023-11-22 22:39:13 +0530

    Time: 12 ms (16.24%), Space: 21.6 MB (73.90%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        // Base case: if the current node is null, return false
+        if (root == nullptr) {
+            return false;
+        }
+
+        // If the current node is a leaf and its value equals the remaining target sum,
+        // then a path with the target sum is found.
+        if (root->left == nullptr && root->right == nullptr && root->val == targetSum) {
+            return true;
+        }
+
+        // Recursively check for the target sum in the left and right subtrees
+        // by subtracting the current node's value from the target sum.
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
+
+
