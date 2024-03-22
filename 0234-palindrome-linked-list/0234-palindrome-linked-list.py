40aa9e13aa04c301cc903103631000de6774f10f
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2024-03-22 19:21:28 +0530

    Time: 274 ms (87.84%), Space: 31.9 MB (85.66%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # If the list is empty or has only one node, it's a palindrome
+        if not head or not head.next:
+            return True
+        
+        # Find the middle of the list using the slow and fast pointers technique
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Reverse the second half of the list
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        # Compare the first half of the list with the reversed second half
+        while prev:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        
+        return True
