cef6f2ce77a57f034fed096185f7cbeb7f189ca4
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2024-03-23 09:42:09 +0530

    Time: 50 ms (84.62%), Space: 24.6 MB (50.88%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head or not head.next:
+            return
+        
+        # Step 1: Find the middle of the linked list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse the second half of the linked list
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        
+        # Step 3: Merge the first half and the reversed second half alternately
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
