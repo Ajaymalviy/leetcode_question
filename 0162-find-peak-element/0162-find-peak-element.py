058c65fa7009123b7e72a3841207e9e1d02a9365
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2024-07-23 22:56:18 +0530

    Update 0162-find-peak-element.py
    added new changes

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
+        
 
         while left < right:
             mid = left + (right - left) // 2
