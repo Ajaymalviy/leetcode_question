8efaea74a74afcf8a850d5379038e75a7842cc97
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2024-08-23 22:08:12 +0530

    Update 0057-insert-interval.cpp
    added new changes

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,7 +4,8 @@ public:
         vector<vector<int>> result;
         int i = 0;
         int n = intervals.size();
-        
+  
+      
         // #If the current interval's ending point is less than the starting point of newInterval, add the current interval to the result array as is because there's no overlap.
   
         // If the current interval's starting point is greater than the ending point of newInterval, it means all remaining intervals will also not overlap with newInterval. In this case, add newInterval to the result array and then add the remaining intervals from intervals to the result array as is.
